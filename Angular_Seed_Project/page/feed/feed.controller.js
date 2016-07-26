83d47493337471366a34db3f82970a0786d6ac23
HenriqueHC <henrique.cavalcante@certsys.com.br>
2016-07-26 15:25:37 -0300

    Post futuro ok, ainda precisa ver o negócio do Infinite Scroll

--- Angular_Seed_Project/page/feed/feed.controller.js
+++ Angular_Seed_Project/page/feed/feed.controller.js
@@ -2,16 +2,20 @@ function feed($scope, $http) {
     var today = new Date();
 
     $scope.isToday = function (date) {
-    	console.log(today);
-    	console.log(date);
-    	return true;
+        // console.log(today);
+        var data = new Date(date);
+        // console.log(data);
+        if (data < today)
+            return true;
+        return false;
     }
 
     $http.get('/posts').then(function (response) {
         $scope.feed = response.data;
     }, console.log("Erro ao pegar os dados"));
     $scope.title = "Newsfeed Certsys";
-    $scope.limit = 1;
+    $scope.limit = 4; // TODO: Problema se a pessoa agendar 5 posts!!!
+
     $scope.loadMore = function () {
         $scope.limit = $scope.limit + 1;
     };
--- Angular_Seed_Project/routes/login.js
+++ Angular_Seed_Project/routes/login.js
@@ -3,35 +3,49 @@ var router = express.Router();
 var ActiveDirectory = require('activedirectory');
 
 
-var config = { url: 'ldap://dc.domain.com',
-               baseDN: 'dc=domain,dc=com',
-               username: 'username@domain.com',
-               password: 'password' }
+var config = {
+    url: 'ldap://192.168.129.2:389',
+    baseDN: 'ou="Certsys",dc="certsys",dc="local"',
+    username: 'svc_intranet@certsys.local',
+    password: 'Cert0104sys'
+}
 
- var ad = new ActiveDirectory(config);
- var username = 'pedro.strabeli@certsys.com.br';
- var password = 'password';
+var ad = new ActiveDirectory(config);
+// var username = 'pedro.strabeli@certsys.com.br';
+var username = 'svc_intranet@certsys.local';
+// var password = 'password';
+var password = 'Cert0104sys';
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	ad.authenticate(username, password, function(err, auth) {
-		  if (err) {
-		    console.log('ERROR: '+JSON.stringify(err));
-		    return;
-		  }
+router.get('/', function (req, res, next) {
+    ad.userExists(username, function(err, exists) {
+        if (err) {
+            res.json(JSON.stringify(err));
+            return;
+        }
 
-		  if (auth) {
-		    console.log('Authenticated!');
-		  }
-		  else {
-		    console.log('Authentication failed!');
-		  }
-	});
+        res.json({data : username   +' exists: '+ exists});
+    });
 
 });
 
+router.post('/', function (req, res) {
+    // ad.authenticate(req.body.username, req.body.password, function (err, auth) {
+    ad.authenticate(username, password, function (err, auth) {
+        if (err) {
+            res.json(JSON.stringify(err));
+            return;
+        }
 
+        if (auth) {
+            res.json({data: 'Entrou!'});
+        }
+        else {
+            res.json({data: 'Falha de autenticação...'});
+        }
+    });
+});
 
 
 module.exports = router;
