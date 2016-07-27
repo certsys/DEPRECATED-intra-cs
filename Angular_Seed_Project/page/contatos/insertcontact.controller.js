2e42972932446d3a01f9197975bc905a4c9a7fa5
LucasArthur94 <lucas.felgueiras@certsys.com.br>
2016-07-27 15:07:55 -0300

    Layout inserção contato corrigido, uso de tags (Ivan Zamperini curtiu isso)

--- Angular_Seed_Project/page/contatos/insertcontact.controller.js
+++ Angular_Seed_Project/page/contatos/insertcontact.controller.js
@@ -1,25 +1,35 @@
 function insertcontacts($scope, $http, $timeout, $state) {
     $scope.title='Novo Contato';
 
-    $scope.tooltable = { 
-            maintools : ['',],
-            tools_basic : ['',],
-            tools_intermediate : ['',],
-            tools_advanced : ['',] 
-        };
+    $scope.maintools = [];
+    $scope.tools_basic = [];
+    $scope.tools_intermediate = [];
+    $scope.tools_advanced = [];
 
-    $scope.addFormFieldMain = function() {
-        $scope.tooltable.maintools.push('');
-    }
-    $scope.addFormFieldBasic = function() {
-        $scope.tooltable.tools_basic.push('');
-    }
-    $scope.addFormFieldInterm = function() {
-        $scope.tooltable.tools_intermediate.push('');
-    }
-    $scope.addFormFieldAdvanc = function() {
-        $scope.tooltable.tools_advanced.push('');
-    }
+    $scope.loadTools = function($query) {
+        return $http.get('tools.json', { cache: true}).then(function(response) {
+            var tools = response.data;
+            return tools.filter(function(tool) {
+                return tool.worktool.toLowerCase().indexOf($query.toLowerCase()) != -1;
+            });
+        });
+    };
+
+    // $scope.addFormFieldMain = function() {
+    //     $scope.tooltable.maintools.push('');
+    // }
+    // $scope.addFormFieldBasic = function() {
+    //     // if (angular.element('#advanced').val() !== "")
+    //         $scope.tooltable.tools_basic.push('');
+    // }
+    // $scope.addFormFieldInterm = function() {
+    //     // if (angular.element('#inter').val() !== "")
+    //         $scope.tooltable.tools_intermediate.push('');
+    // }
+    // $scope.addFormFieldAdvanc = function() {
+    //     // if (angular.element('#basic').val() !== "")
+    //         $scope.tooltable.tools_advanced.push('');
+    // }
 
     $scope.thumbnail = [];
     $scope.fileReaderSupported = window.FileReader != null;
@@ -42,12 +52,41 @@ function insertcontacts($scope, $http, $timeout, $state) {
 
 
     $scope.submit = function () {
-        
+        var basic_array = [];
+        var inter_array = [];
+        var advanced_array = [];
+        var log = [];
+
+        for(var i = 0; i < $scope.tools_basic.length; i++) {
+            angular.forEach($scope.tools_basic[i], function(value, key) {
+                if (key != "$$hashKey") basic_array.push(value);
+            }, log);
+        }
+
+        for(var i = 0; i < $scope.tools_intermediate.length; i++) {
+            angular.forEach($scope.tools_intermediate[i], function(value, key) {
+                if (key != "$$hashKey") inter_array.push(value);
+            }, log);
+        }
+
+        for(var i = 0; i < $scope.tools_advanced.length; i++) {
+            console.log($scope.tools_advanced[i]);
+            angular.forEach($scope.tools_advanced[i], function(value, key) {
+                if (key != "$$hashKey") advanced_array.push(value);
+            }, log);
+        }
+
+        var tooltable = {
+            maintools : $scope.maintools,
+            tools_basic : basic_array,
+            tools_intermediate : inter_array,
+            tools_advanced : advanced_array
+        };
 
         var data = {
             nome: $scope.name,
             sobre: $scope.sobre,
-            tooltable: $scope.tooltable,
+            tooltable: tooltable,
             mail: $scope.mail,
             phone: $scope.phone,
             skype: $scope.skype,
@@ -56,18 +95,18 @@ function insertcontacts($scope, $http, $timeout, $state) {
 
         var output = JSON.stringify(data);
         console.log(output);
-        $http({method: 'POST', url:'/contacts', data: output})
-        	.then(function(response){
-        		//your code in case the post succeeds
-        		//console.log(response);
-                alert('Contato inserido com sucesso.');
-                $state.go('contatos');
-        	})
-        	.catch(function(err){
-        		//your code in case your post fails
-        		//console.log(err);
-                alert('Ocorreu um erro. Contato não adicionado. \nErro: '+JSON.stringify(err));
-        	});
+        // $http({method: 'POST', url:'/contacts', data: output})
+        // 	.then(function(response){
+        // 		//your code in case the post succeeds
+        // 		//console.log(response);
+        //         alert('Contato inserido com sucesso.');
+        //         $state.go('contatos');
+        // 	})
+        // 	.catch(function(err){
+        // 		//your code in case your post fails
+        // 		//console.log(err);
+        //         alert('Ocorreu um erro. Contato não adicionado. \nErro: '+JSON.stringify(err));
+        // 	});
     }
     
 };
