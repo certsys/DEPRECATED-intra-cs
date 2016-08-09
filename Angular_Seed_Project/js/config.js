df45c7ed3e203e4e395f3f330f2d0bb23fcae14c
Ivan Romero Zoppetti <ivan.zoppetti@certsys.local>
2016-08-09 19:52:57 -0300

    nova página de políticas dentro de institucional, falta conteúdo, mas tá do jeito 666

--- Angular_Seed_Project/js/config.js
+++ Angular_Seed_Project/js/config.js
@@ -70,6 +70,40 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $locati
                 }
             }
         })
+	
+	
+	
+    	
+	
+	.state('politicas', {
+            url: "/institucional/politicas"
+            , templateUrl: "page/institucional/politicas/politicas.html"
+            , data: {
+                pageTitle: 'Políticas'
+            }
+            , resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/pace.min.js',
+                                'js/wow.min.js',
+                                'js/classie.js',
+                                'js/cbpAnimatedHeader.js',
+								'css/animate.css',
+								'js/plugins/slimscroll/jquery.slimscroll.min.js'
+								
+									
+
+
+                            ]
+                        }
+                    ]);
+                }
+            }
+        })
+	
+	
+	
 
         .state('insertnews', {
             url: "/insertnews"
