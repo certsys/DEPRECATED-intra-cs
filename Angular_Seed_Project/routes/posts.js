450a8e2bcdb4df79c9988b1c4f21dee1daf791d7
HenriqueHC <henrique.cavalcante@certsys.com.br>
2016-08-22 14:26:46 -0300

    Encriptação de senha do email para salvar no banco

--- Angular_Seed_Project/routes/posts.js
+++ Angular_Seed_Project/routes/posts.js
@@ -6,6 +6,23 @@ var async = require('async');
 var nodemailer = require('nodemailer');
 var schedule = require('node-schedule');
 var smtpTransport = require('nodemailer-smtp-transport');
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'Cert0104sys';
+
+function encrypt(text) {
+    var cipher = crypto.createCipher(algorithm, password)
+    var crypted = cipher.update(text, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text) {
+    var decipher = crypto.createDecipher(algorithm, password)
+    var dec = decipher.update(text, 'hex', 'utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
 
 var mails = [];
 var posting = [];
@@ -38,15 +55,22 @@ router.get('/reschedule', function (req, res) {
     Post.find(function (err, posts) {
         if (err) return console.error(err);
         var data_atual = new Date();
-        posts.forEach(function (post){
-            var transporter = nodemailer.createTransport(smtpTransport(post.mail_transport));
+        posts.forEach(function (post) {
             if (post.mail_transport) {
+
+                var options = {
+                    host: 'webmail.exchange.locaweb.com.br',
+                    port: 587, // Porta SMTP no Exchange
+                    auth: {
+                        user: post.mail_transport.auth.user, // Colocar e-mail do RH aqui;
+                        pass: decrypt(post.mail_transport.auth.pass) // Senha do e-mail aqui;
+                    }
+                };
+                var transporter = nodemailer.createTransport(smtpTransport(options));
                 transporter.verify(function (error, success) {
                     if (error) return console.error(error);
                     else {
                         if (post.data >= data_atual && post.mail) {
-
-                            console.log(post.titulo);
                             // console.log("Post Futuro");
 
                             var email = post.mail;
@@ -183,6 +207,14 @@ router.post('/', function (req, res) {
                     }]
                 };
 
+                options = {
+                    host: 'webmail.exchange.locaweb.com.br',
+                    port: 587, // Porta SMTP no Exchange
+                    auth: {
+                        user: req.body.usermail, // Colocar e-mail do RH aqui;
+                        pass: encrypt(req.body.password) // Senha do e-mail aqui;
+                    }
+                };
 
                 var newPost = new Post({
                     mail: email,
@@ -373,53 +405,5 @@ router.put('/edit/:id', function (req, res, next) {
 router.get('/:post', function (req, res) {
     res.json(req.post);
 });
-
-var create_mail = function (assinatura_mail, usermail, titulo, texto, imagem) {
-    var assinatura = assinatura_mail;
-    if (assinatura.indexOf('-') > -1)
-        assinatura = req.body.assinatura.substr(0, assinatura.indexOf('-'));
-    var assinaturaEmail = '<div><p style="font-family: Helvetica, Arial, sans-serif; font-size: 12px; line-height: 14px; color: rgb(33, 33, 33); margin-bottom: 10px;"><span style="font-weight: bold; color: rgb(134, 134, 134); display: inline;">' + req.body.sendBy + '</span><span style="display: inline;"> | </span> <span style="color: rgb(134, 134, 134); display: inline;">' + assinatura + '</span><span style="display: inline;"><br></span><a href="mailto:' + req.body.usermail + '" style="color: rgb(14, 76, 170); text-decoration: none; display: inline;">' + req.body.usermail + '</a><span style="display: inline;"><p style="font-family: Helvetica, Arial, sans-serif; font-size: 12px; line-height: 14px; margin-bottom: 10px;"><span style="font-weight: bold; color: rgb(134, 134, 134); display: inline;">Certsys Tecnologia da Informação Ltda</span><span style="display: inline;"><br></span><span style="color: rgb(134, 134, 134); display: inline;"> + 55 11 5084-2984</span><span style="display: inline;"><br></span> <span style="color: rgb(134, 134, 134); display: inline;">Rua Estela 515 Conjunto F - 22</span><span></span><span style="display: inline;"><br></span><a href="http://www.certsys.com.br" style="color: rgb(14, 76, 170); text-decoration: none; display: inline;">www.certsys.com.br</a>  </p><p style="font-family: Helvetica, Arial, sans-serif; font-size: 12px; line-height: 14px; color: rgb(33, 33, 33); margin-bottom: 10px;"><span style="color: rgb(134, 134, 134); display: inline;"><a alt="Facebook" style="text-decoration: none; display: inline;" href="https://www.facebook.com/certsys" target="_blank"> Facebook </a></span><span style="display: inline;">|</span><span style="color: rgb(134, 134, 134); display: inline;"><a alt="Facebook" style="text-decoration: none; display: inline;" href="https://www.linkedin.com/company/certsys-tecnologia-da-informa-o" target="_blank"> LinkedIn </a></span></div>';
-    var email = {
-        from: usermail, // Quem enviou este e-mail
-        to: 'henrique_hashimoto@hotmail.com, henrique.hashimoto.cavalcante@usp.br', // Quem receberá (todos@certsys.com.br)
-        subject: titulo,  // Um assunto
-        html: '<img src="cid:imagemDoPost"/><br><br>' + texto + '<br>' + assinaturaEmail, // O conteúdo do e-mail
-        attachments: [{
-            filename: 'image.png',
-            path: imagem,
-            cid: 'imagemDoPost'
-        }]
-    };
-
-
-    if (req.body.data) {
-        var date = new Date(req.body.data);
-        var agendamento = schedule.scheduleJob(date, function () {
-            transporter.sendMail(email, function (err, info) {
-                if (err)
-                    console.error(err);
-
-            });
-        });
-        posting.push(newPost);
-        mails.push(agendamento);
-        res.json(
-            {
-                status: true,
-                mail: true
-            });
-    }
-    else {
-        transporter.sendMail(email, function (err, info) {
-            if (err)
-                console.error(err);
-            res.json(
-                {
-                    status: true,
-                    mail: true
-                });
-        });
-    }
-};
 
 module.exports = router;
