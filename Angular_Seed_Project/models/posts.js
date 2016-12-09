6b5859197354bfff8972a55bba7e5813bf89a03a
Marcos Hosoya <marcos.hosoya@certsys.com.br>
2016-12-09 18:51:43 -0200

    refactor(comments) : Remove comments
    Remove comments
    
    #9

--- Angular_Seed_Project/models/posts.js
+++ Angular_Seed_Project/models/posts.js
@@ -14,31 +14,3 @@ var postSchema = new mongoose.Schema({
 var Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
-
-// //utility functions
-// var Post = mongoose.model('Post');
-// exports.findByUsername = function(userName, callback){
-//
-//     User.findOne({ user_name: userName}, function(err, user){
-//
-//         if(err){
-//             return callback(err);
-//         }
-//
-//         //success
-//         return callback(null, user);
-//     });
-//
-// }
-//
-// exports.findById = function(id, callback){
-//
-//     User.findById(id, function(err, user){
-//
-//         if(err){
-//             return callback(err);
-//         }
-//
-//         return callback(null, user);
-//     });
-// }
